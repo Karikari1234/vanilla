95018691cb6d01fc31933707251d976cb3d08429
nafiz <naafizrahman1@gmail.com>
2021-06-06 05:34:03 +0600

    New changes

--- play.js
+++ play.js
@@ -104,3 +104,20 @@ L.marker([23.8103, 90.4125])
   .addTo(map)
   .bindPopup("A pretty CSS3 popup.<br> Easily customizable.")
   .openPopup();
+
+  const webcamElement = document.getElementById('webcam');
+const canvasElement = document.getElementById('canvas');
+const snapSoundElement = document.getElementById('snapSound');
+const webcam = new Webcam(webcamElement, 'user', canvasElement, snapSoundElement);
+
+webcam.start()
+  .then(result =>{
+    console.log("webcam started");
+  })
+  .catch(err => {
+    console.log(err);
+});
+
+
+let picture = webcam.snap();
+document.querySelector('#download-photo').href = picture;
